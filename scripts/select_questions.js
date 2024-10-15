0c0e36eaff5b1b4afa368522fe96e458675738cd
lexuanbinh <lb@silverliningresearch.com>
2024-10-15 16:40:16 +0700

    update new files

--- scripts/select_questions.js
+++ scripts/select_questions.js
@@ -1,46 +1,12 @@
 let selected_items = [];
 
-function get_the_minimum(arr)
-{
-  //sort
-  arr = arr.sort((a, b) => a[1] - b[1]);
-  
-  console.log("input - sorted: ", arr);
-
-  selected_items = [];
-  selected_items[0] = arr[0][0];
-
-  //identify the candidate list
-  var max = 0;
-  for (let i = 0; i < arr.length-1; i++) {
-    if (arr[i][1] == arr[i+1][1]) {
-      selected_items[i+1] = arr[i+1][0];
-      max = i+1;
-    }
-    else break;
-  }
-
-  var random = Math.random();
-  var random_round = Math.floor(random*(max+1)); // Returns a random integer from 0 to max index + 1:
-  console.log("selected_items: ", selected_items); 
-
-  console.log("max: ", max); 
-  console.log("random: ", random); 
-  console.log("random_round: ", random_round); 
-
-  var selected_index = random_round;
-  console.log("selected index: ", selected_index); 
-
-  var result = 0;
-  if (arr[selected_index][1] < 4) result = arr[selected_index][0];
-
-  console.log("result: ", result); 
-
-  return (result);
+function shuffleArray(array) {
+  return array.sort(() => Math.random() - 0.5);
 }
 
 function get_selection()
 {
+  $('.rt-btn.rt-btn-next').hide(); 
   
   let selected_id = [];
   let selected_label = [
@@ -87,27 +53,33 @@ function get_selection()
   if ((api.fn.answers().Vilka_aktiviteter_19) && (api.fn.answers().Vilka_aktiviteter_19 == 1)) selected_id.push(19);
   if ((api.fn.answers().Vilka_aktiviteter_20) && (api.fn.answers().Vilka_aktiviteter_20 == 1)) selected_id.push(20);
 
-  console.log("selected item: ", selected_id); 
+
+  //var selected_id_random = shuffleArray(selected_id);
+
+  var selected_id_random = selected_id; //dont need to shuffle as there is maximum 3 only.
   
-  if (selected_id[0] && selected_id[0]>0) {
-    api.fn.answers({selected_one:  selected_label[selected_id[0]-1]}); //index from 0
+  console.log("selected item: ", selected_id_random); 
+  
+  if (selected_id_random[0] && selected_id_random[0]>0) {
+    api.fn.answers({selected_one:  selected_label[selected_id_random[0]-1]}); //index from 0
   }
   else {
     api.fn.answers({selected_one:  "NA"}); //index from 0
   }
 
-  if (selected_id[1] && selected_id[1]>0) {
-    api.fn.answers({selected_two:  selected_label[selected_id[1]-1]});
+  if (selected_id_random[1] && selected_id_random[1]>0) {
+    api.fn.answers({selected_two:  selected_label[selected_id_random[1]-1]});
   }
   else {
     api.fn.answers({selected_two:  "NA"}); //index from 0
   }
 
-  if (selected_id[2] && selected_id[2]>0) {
-    api.fn.answers({selected_three:  selected_label[selected_id[2]-1]});
+  if (selected_id_random[2] && selected_id_random[2]>0) {
+    api.fn.answers({selected_three:  selected_label[selected_id_random[2]-1]});
   }
   else {
     api.fn.answers({selected_three:  "NA"}); //index from 0
   }
 
+  $('.rt-btn.rt-btn-next').show(); 
 }
